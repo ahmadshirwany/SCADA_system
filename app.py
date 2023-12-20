34e575073a7538907f53eadccb5eb8333c594ccf
Ahmad Ali Khan Shirwany <shirwany.shirwany@gmail.com>
2023-12-20 16:10:18 +0200

    adding requirments.txt

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request
-from controllers import  DataService,MQTTService
+from controllers import DataService, MQTTService
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
 
+
 @app.route('/')
 def display_data():
     data = data_service.fetch_all_data()
-    return render_template('index.html', data=data)
+    robot_ids = []
+    default_robot_id = 'rob1'
+    return render_template('dashboard.html', data=data, robot_ids=robot_ids, nID=default_robot_id)
 
 
 @app.route('/data')
@@ -16,11 +19,13 @@ def data():
     data = data_service.fetch_all_data(robtID)
     return data
 
+
 @app.route('/rob_data_stats')
 def get_robot():
     robtID = request.args.get('robtID', default=None)
     data = data_service.rob_data_stats(robtID)
     return data
+
 
 @app.route('/robots_latest_statu')
 def get_robot_latast_status():
