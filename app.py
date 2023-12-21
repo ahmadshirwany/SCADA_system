f4c1b67b4ccdd145dc772f79644e84ac0a7350e3
Ahmad Ali Khan Shirwany <shirwany.shirwany@gmail.com>
2023-12-21 14:04:50 +0200

    updating functionalities

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 from controllers import DataService, MQTTService
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
 
 
